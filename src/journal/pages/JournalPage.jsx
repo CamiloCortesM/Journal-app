b5922292beb7c9a0b75262953a548bf735a86b7f
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-19 00:20:40 -0500

    implementing ui material

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,11 @@
+import { MailOutline } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
-  )
-}
+    <>
+      <Typography>JournalPage</Typography>
+      <MailOutline />
+    </>
+  );
+};
