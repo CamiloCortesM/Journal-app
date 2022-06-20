a834043037d6e83e328272d5589134df3face53e
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-19 23:21:14 -0500

    layout for the pages journal was created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,14 @@
-import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Pariatur irure laborum occaecat amet nulla qui nostrud labore do irure
+        irure. Eiusmod tempor esse eiusmod deserunt ex ut laboris magna. Mollit
+        commodo cillum labore est id irure ipsum eu adipisicing et et magna.
+      </Typography>
+    </JournalLayout>
   );
 };
