6c1f8facd8b701911e5cf1e0c531d19ffdd3c492
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-20 00:55:40 -0500

    new floating add journal button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NoteView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
@@ -10,9 +12,23 @@ export const JournalPage = () => {
         commodo cillum labore est id irure ipsum eu adipisicing et et magna.
       </Typography> */}
 
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
-      <NoteView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
