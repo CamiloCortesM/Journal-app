89986dd7e6e22ef3f3c8b4126ce27bf71df13a2a
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-20 00:08:10 -0500

    SideBar creation of the application

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,24 +1,21 @@
-import { Box } from "@mui/system"
-import { NavBar } from "../components";
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
-export const JournalLayout = ({children}) => {
+export const JournalLayout = ({ children }) => {
   return (
-    <Box sx={{display: 'flex'}}>
-        	<NavBar drawerWidth={drawerWidth}/>
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
 
-            {/* Sidebar */}
+      <SideBar drawerWidth={drawerWidth} />
 
-        <Box 
-        component='main'
-        sx={{flexGrow:1,p:3}}
-        >
-            
-            {/* Toolbar */}
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
 
-            { children }
-        </Box>
+        {children}
+      </Box>
     </Box>
-  )
-}
+  );
+};
