8bb7c8549bd26081f668b946a1b8f49634b19000
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-07-13 19:08:14 -0500

    redux toolkit was downloaded

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { JornalApp } from './JornalApp'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { JornalApp } from "./JornalApp";
+import { store } from "./store";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <JornalApp />
+    <Provider store={store}>
+      <JornalApp />
+    </Provider>
   </React.StrictMode>
-)
+);
