f2b0e6e445553726877ce6455f9bc9d41264670a
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-07-17 01:38:52 -0500

    condition for displaying the load component has been added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,18 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-
+import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") return <CheckingAuth />;
   return (
     <Routes>
-         <Route path="/auth/*" element={<AuthRoutes/>}/>
+      <Route path="/auth/*" element={<AuthRoutes />} />
 
-         <Route path="/*" element={<JournalRoutes/>}/>
+      <Route path="/*" element={<JournalRoutes />} />
     </Routes>
-  )
-}
+  );
+};
