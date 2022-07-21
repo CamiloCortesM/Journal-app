d7fda443f397fe40ea44704b4590db7a4220b0dc
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-07-21 14:46:54 -0500

    the notes were placed in the sidebar and a new style was added to the title

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,21 +1,10 @@
 import { useSelector } from "react-redux";
-import { TurnedInNot } from "@mui/icons-material";
-import {
-  Box,
-  Divider,
-  Drawer,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material";
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({ drawerWidth }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
 
   return (
     <Box
@@ -38,22 +27,8 @@ export const SideBar = ({ drawerWidth }) => {
         <Divider />
 
         <List>
-          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      "Et pariatur voluptate culpa consequat laboris cillum aute elit labore labore laboris nisi culpa anim."
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SideBarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
