72784b683c3531774e7eacaafad176d711855469
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-08-09 23:32:07 -0500

    database for testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,25 @@ import { getFirestore } from "firebase/firestore/lite";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+//Dev/Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCLWsIQ_cuC9RNQk1CxBwVwdCEJdy2fZzo",
+//   authDomain: "react-cursos-eeb8f.firebaseapp.com",
+//   projectId: "react-cursos-eeb8f",
+//   storageBucket: "react-cursos-eeb8f.appspot.com",
+//   messagingSenderId: "948113951542",
+//   appId: "1:948113951542:web:77675fd2c0e87a1efb0197",
+// };
+
+//Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyCLWsIQ_cuC9RNQk1CxBwVwdCEJdy2fZzo",
-  authDomain: "react-cursos-eeb8f.firebaseapp.com",
-  projectId: "react-cursos-eeb8f",
-  storageBucket: "react-cursos-eeb8f.appspot.com",
-  messagingSenderId: "948113951542",
-  appId: "1:948113951542:web:77675fd2c0e87a1efb0197",
+  apiKey: "AIzaSyD4FibmvHjsgQ7ndbB6VPVbYs3n7tjy7SU",
+  authDomain: "testing-eb245.firebaseapp.com",
+  projectId: "testing-eb245",
+  storageBucket: "testing-eb245.appspot.com",
+  messagingSenderId: "1034242638894",
+  appId: "1:1034242638894:web:f495d1846edcb4511a8bbf",
+  measurementId: "G-5JRZVBEFKL"
 };
 
 // Initialize Firebase
