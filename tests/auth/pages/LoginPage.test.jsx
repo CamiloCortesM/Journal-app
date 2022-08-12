272f4431fe53f642f4b5ed67baff850d23c2833d
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-08-11 23:59:14 -0500

    test in LoginPage

--- tests/auth/pages/LoginPage.test.jsx
+++ tests/auth/pages/LoginPage.test.jsx
@@ -1,15 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { LoginPage } from "../../../src/auth/pages/LoginPage";
 import { authSlice } from "../../../src/store/auth";
+import { notAuthenticatedState } from "../../fixtures/authFixtures";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
   },
-  //   preloadedState
+  preloadedState: {
+    auth: notAuthenticatedState,
+  },
 });
 
 describe("Test in LoginPage", () => {
@@ -25,5 +28,18 @@ describe("Test in LoginPage", () => {
 
     expect(screen.getAllByText("Login").length).toBeGreaterThanOrEqual(1);
     expect(container).toMatchSnapshot();
+  });
+
+  test("button of google must called startGoogleSingIn", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const googleBtn = screen.getByLabelText("google-btn");
+    fireEvent.click(googleBtn);
   });
 });
